40bfa226cab3c7e814fa84f5717ea1adb2822b24
Fancyiscrying <Fancy@fancydeair.mshome.net>
2016-03-01 11:03:11 +0800

    Initial Commit

---
+++ NSNotificationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSNotificationTest
+//
+//  Created by Fancy on 16/3/1.
+//  Copyright © 2016年 Fancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSNotificationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSNotificationTest
+//
+//  Created by Fancy on 16/3/1.
+//  Copyright © 2016年 Fancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
